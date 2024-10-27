9ba1b3471816a4bdfcc2c3714660dce590bac535
aldanarastrelli-peya <aldanarastrelli@gmail.com>
2024-10-27 13:06:31 -0300

    Create main.rs for example run

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use raft::{node_config::{Node, NodesConfig}, raft_module::RaftModule};
+use std::env;
+
+#[actix_rt::main]
+async fn main() {
+    // cargo run node1 5433
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("Usage: {} <node_id> <total_nodes>", args[0]);
+        std::process::exit(1);
+    }
+
+    let node_id = args[1].to_string();
+
+    let port: usize = args[2].parse().expect("Invalid port, must be a number");
+
+    let mut raft_node = RaftModule::new(node_id, "127.0.0.1".to_string(), port);
+
+    // TODO this is for local testing. Delete this
+    let nodes = NodesConfig {
+        nodes: vec![
+            Node {
+                ip: "127.0.0.1".to_string(),
+                port: "5433".to_string(),
+                name: "node1".to_string(),
+            },
+            Node {
+                ip: "127.0.0.1".to_string(),
+                port: "5434".to_string(),
+                name: "node2".to_string(),
+            },
+            Node {
+                ip: "127.0.0.1".to_string(),
+                port: "5435".to_string(),
+                name: "node3".to_string(),
+            },
+            Node {
+                ip: "127.0.0.1".to_string(),
+                port: "5436".to_string(),
+                name: "node4".to_string(),
+            },
+        ],
+    };
+
+    raft_node.start(nodes).await;
+}
